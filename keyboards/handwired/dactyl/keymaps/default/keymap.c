bb1e9a9ed0d0df76e060895a78481b2b672760b4
Andy Yaco-Mink <andy@yacomink.com>
2019-07-18 15:13:03 -0400

    well the esc hold thing don't work

--- keyboards/handwired/dactyl/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl/keymaps/default/keymap.c
@@ -35,20 +35,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_dactyl(  // layer 0 : default
         // left hand
-          MT(KC_ESC, KC_EQL), KC_1,    KC_2,     KC_3,     KC_4,  KC_5,
+          KC_EQL,             KC_1,    KC_2,     KC_3,     KC_4,  KC_5,
           KC_TAB,             KC_Q,    KC_W,     KC_E,     KC_R,  KC_T,
           KC_LCTL,            KC_A,    KC_S,     KC_D,     KC_F,  KC_G,
           KC_LSFT,            KC_Z,    KC_X,     KC_C,     KC_V,  KC_B,
       VSCODE_KEEP,            KC_GRV,  KC_BSLS,  KC_LEFT,  KC_RGHT,
-                                                                            KC_LGUI, KC_LALT,
-                                                                                      KC_HOME,
-                                                             KC_BSPC,        KC_DELT,   KC_END,
+                                                                      KC_LGUI,  KC_LALT,
+                                                                                KC_HOME,
+                                                           KC_BSPC,   KC_DELT,   KC_END,
         // right hand
-                             KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         KC_MINS,
-                             KC_Y,   KC_U,     KC_I,     KC_O,               KC_P,         KC_BSLS,
-                             KC_H,   KC_J,     KC_K,     KC_L,  KC_SCLN,         KC_QUOT,
-                             KC_N,   KC_M,  KC_COMM,   KC_DOT,     KC_SLSH,         KC_RSFT,
-                                    KC_UP,  KC_DOWN,  KC_LBRC,            KC_RBRC,          MO(SYMB),
+                             KC_6,    KC_7,     KC_8,     KC_9,       KC_0,         KC_MINS,
+                             KC_Y,    KC_U,     KC_I,     KC_O,       KC_P,         KC_BSLS,
+                             KC_H,    KC_J,     KC_K,     KC_L,       KC_SCLN,      KC_QUOT,
+                             KC_N,    KC_M,     KC_COMM,  KC_DOT,     KC_SLSH,      KC_RSFT,
+                                      KC_UP,    KC_DOWN,  KC_LBRC,    KC_RBRC,      MO(SYMB),
           KC_RALT,  KC_ESC,
           KC_PGUP,
           KC_PGDN, KC_ENT, KC_SPC
